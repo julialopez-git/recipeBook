a3cd88239dce821941c0d0ecbe55a3168b937790
julia lopez <julia.lopez@globant.com>
2022-02-09 10:59:56 -0300

    Created Category entity and added many to many relationship with Recipe. Closes #7

--- src/main/java/julia/recipeWebApp/models/Recipe.java
+++ src/main/java/julia/recipeWebApp/models/Recipe.java
@@ -33,6 +33,16 @@ public class Recipe {
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients = new HashSet<>();
 
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+            joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
+
+
+    /*----------- getters and setters -----------------*/
+
+
     public Notes getNotes() {
         return notes;
     }
@@ -129,5 +139,13 @@ public class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
